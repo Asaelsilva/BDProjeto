ae0d881bcab6992fb3d9b8be511d07277fb421c2
asael joao <asael.guitar_g3@hotmail.com>
2019-08-17 15:09:52 -0300

    Melhorando Crud

--- ConexaoBD/Program.cs
+++ ConexaoBD/Program.cs
@@ -7,15 +7,11 @@ using System.Data.SqlClient;
 
 namespace ConexaoBD
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
             var bd = new Bd();
-            var usuarioAplicacao = new UsuarioAplicacao();
-
-            //SqlConnection conexao = new SqlConnection(@"data source=; Integrated Security=; Initial Catalog=");
-            //conexao.Open();
 
             Console.WriteLine("Digite o nome do uruario: ");
             string nome = Console.ReadLine();
@@ -26,43 +22,24 @@ namespace ConexaoBD
             Console.WriteLine("Digite o data: ");
             string data = Console.ReadLine();
 
-            usuarioAplicacao.Insert(nome, cargo, data);
+            Usuarios usuarios = new Usuarios
+            {
+                Nome = nome,
+                Cargo = cargo,
+                Data = DateTime.Parse(data)
+                
+            };
 
-            string strQuerySelect = "SELECT * FROM USUARIOS";
+            usuarios.Id = 17;
+            new UsuarioAplicacao().Salvar(usuarios);
+
+            string strQuerySelect = "SELECT * FROM usuarios";
             SqlDataReader dados = bd.ExecutaComandoComRetorno(strQuerySelect);
 
             while (dados.Read())
             {
                 Console.WriteLine("id:{0}, nome:{1}, cargo:{2}, data:{3}", dados["usuarioid"], dados["nome"], dados["cargo"], dados["date"]);
             }
-            
-
-            //string strQueryUpdate = "UPDATE usuarios set nome = 'Larissa' WHERE usuarioId = 6";
-            //SqlCommand cmdComandoUpdate = new SqlCommand(strQueryUpdate, conexao);
-            //cmdComandoUpdate.ExecuteNonQuery();
-
-            //string strQueryDelete = "DELETE FROM usuarios WHERE usuarioId = 9";
-            //SqlCommand cmdComandoDelete = new SqlCommand(strQueryDelete, conexao);
-            //cmdComandoDelete.ExecuteNonQuery();
-
-            //Console.WriteLine("Digite o nome do uruario: ");
-            //string nome = Console.ReadLine();
-
-            //Console.WriteLine("Digite o cargo: ");
-            //string cargo = Console.ReadLine();
-
-            //Console.WriteLine("Digite o data: ");
-            //string data = Console.ReadLine();
-
-            //string strQueryInsert = string.Format("INSERT INTO usuarios(nome, cargo, date) VALUES('{0}', '{1}', '{2}')", nome, cargo, data);
-            //SqlCommand cmdComandoInsert = new SqlCommand(strQueryInsert, conexao);
-            //cmdComandoInsert.ExecuteReader();
-
-            //string strQuerySelect = "SELECT * FROM USUARIOS";
-            //SqlCommand cmdComandoSelect = new SqlCommand(strQuerySelect, conexao);
-            //SqlDataReader dadosSelect = cmdComandoSelect.ExecuteReader();
-
-
         }
     }
 }
--- ConexaoBD/UsuarioAplicacao.cs
+++ ConexaoBD/UsuarioAplicacao.cs
@@ -10,18 +10,58 @@ namespace ConexaoBD
     {
         private Bd bd;
 
-        public void Insert(string nome, string cargo, string data)
+        private void Inserir(Usuarios usuario)
         {
-            var strQuery = " ";
+            var strQuery = "";
             strQuery += "INSERT INTO usuarios(nome, cargo, date)";
-            strQuery += string.Format(" VALUES ('{0}', '{1}', '{2}')", nome, cargo, data
+            strQuery += string.Format(" VALUES ('{0}', '{1}', '{2}')", usuario.Nome, usuario.Cargo, usuario.Data
                 );
 
-            using(bd = new Bd())
+            using (bd = new Bd())
             {
                 bd.ExecutaComando(strQuery);
             }
 
         }
+
+        private void Alterar(Usuarios usuarios)
+        {
+            var strQuery = "";
+            strQuery += "UPDATE usuarios SET ";
+            strQuery += string.Format("nome = '{0}',", usuarios.Nome);
+            strQuery += string.Format("cargo = '{0}',", usuarios.Cargo);
+            strQuery += string.Format("date = '{0}' ", usuarios.Data);
+            strQuery += string.Format("WHERE usuarioId = {0} ", usuarios.Id);
+
+            using (bd = new Bd())
+            {
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        public void Salvar(Usuarios usuarios)
+        {
+            if (usuarios.Id > 0 )
+            {
+                Alterar(usuarios);
+            }
+            else
+            {
+                Inserir(usuarios);
+            }
+        }
+
+
+        public void Excluir(int id)
+        {
+            using(bd =new Bd())
+            {
+                var strQuery = string.Format(" DELETE FROM usuarios WHERE susuarios = {0}", id);
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+
     }
+
 }
